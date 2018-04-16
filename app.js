9df63fa4185e49fbafdebd3e468ebbff2f77ad2c
Jessenia Guapisaca <jessenia.guapisaca@f1006-19-34183.bmcc.cuny.edu>
2018-04-16 19:12:26 -0400

    update js

--- app.js
+++ app.js
@@ -1,41 +1,32 @@
-// document ready event
 $(document).ready(function() {
-	$('#query').on("keypress", function(event) {
+	$('#query').on("keypress", function(event){
 		if (event.which == 13) {
-			var offset = Math.round(Math.random() * 1000);
 			var query = this.value;
-			var key = "iAqAbqIuPD5ZN9uEq5v3phweMO2u9tda";
+			var key = "yIIDp3OQJ4ti8c0zn7IaKbR6alw5wkoJ";
 			var url = "https://api.giphy.com/v1/gifs/search?q="
-						+ query
-						+ "&api_key="
-						+ key
-						+ "&limit=20"
-						+ "&offset="
-						+ offset;
-			
+				+ query 
+				+ "&api_key=" 
+				+ key 
+				+ "&limit=20";
+
 			$.getJSON(url, function(json) {
 				console.log(json);
-				document.getElementById('gifs').innerHTML = "";
 				for (let i = 0; i < json.data.length; i++) {
-					const img = json.data[i];
-					if (img.images.downsized.url) {
-						const imgElem = $('<img>')
-							.attr('src', img.images.downsized.url);
-					
-						const imgContainer = $('<div>')
-							.addClass('gif');
-					
-						imgContainer.append(imgElem);
-					
-						 $('#gifs').append(imgContainer);
+				const img = json.data[i];
+				const imgElem = $('<img>')
+					.attr('src', img.images.downsized.url);
+					$('#gifs').append(imgElem);
+
 					}
-				}
+		
 			});
-		}
+
+		} 
+
+
 	});
+
+
 });
-
-
-
 
 
