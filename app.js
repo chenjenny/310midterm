07ec2ea173a5b377917c045f58ed8468e804a187
Jessenia Guapisaca <jessenia.guapisaca@f1006-19-34183.bmcc.cuny.edu>
2018-04-16 20:01:21 -0400

    update html

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 $(document).ready(function() {
-<<<<<<< HEAD
 	$('#query').on("keypress", function(event){
 		if (event.which == 13) {
-=======
-	$('#query').on("keypress", function(event) {
-		if (event.which == 14) {
-			var offset = Math.round(Math.random() * 1000);
->>>>>>> origin/master
 			var query = this.value;
 			var key = "yIIDp3OQJ4ti8c0zn7IaKbR6alw5wkoJ";
 			var url = "https://api.giphy.com/v1/gifs/search?q="
 				+ query 
 				+ "&api_key=" 
 				+ key 
-				+ "&limit=20";
+				+ "&limit=10";
 
 			$.getJSON(url, function(json) {
 				console.log(json);
