440d1b7a7656a695a5e4200d262aa80dbcdb89f5
Jessenia Guapisaca <jessenia.guapisaca@f1006-19-34183.bmcc.cuny.edu>
2018-04-18 19:09:18 -0400

    update images

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ $(document).ready(function () {
 		var key = "iAqAbqIuPD5ZN9uEq5v3phweMO2u9tda";
 		var nounQuery = document.getElementById("queryOne").value;
 		// verb 
-		var verbQuery = document.getElementById("queryOne").value;
+		var verbQuery = document.getElementById("queryTwo").value;
 		// adj
-		var adjectiveQuery = document.getElementById("queryOne").value;
+		var adjQuery = document.getElementById("queryThree").value;
 
 		var nounUrl = "https://api.giphy.com/v1/gifs/search?q=" +
 			nounQuery +
@@ -20,37 +20,53 @@ $(document).ready(function () {
 			"&limit=1";
 		
 		var verbUrl = "https://api.giphy.com/v1/gifs/search?q=" +
-			nounQuery +
+			verbQuery +
 			"&api_key=" +
 			key
 			//Limits the # of gif you want shown, currently 1 gif per section
 			+
 			"&limit=1";
 		
-			var adjectiveUrl = "https://api.giphy.com/v1/gifs/search?q=" +
-			nounQuery +
+			var adjUrl = "https://api.giphy.com/v1/gifs/search?q=" +
+			adjQuery +
 			"&api_key=" +
 			key
 			//Limits the # of gif you want shown, currently 1 gif per section
 			+
 			"&limit=1";
-		
+		console.log(nounUrl)
 		$.getJSON(nounUrl, function (nounData) {
 			$.getJSON(verbUrl, function (verbData) {
 				$.getJSON(adjUrl, function (adjData) {
 					document.getElementById('gifsOne').innerHTML = "";
 					document.getElementById('gifsTwo').innerHTML = "";
 					document.getElementById('gifsThree').innerHTML = "";
-					
+					console.log(nounData)
 					const nounImg = $('<img>')
-							.attr('src', nounData.data.images[0].downsized.url);
-					const imgContainertwo = $('<div>')
+							.attr('src', nounData.data[0].images.downsized.url);
+					const imgContainerOne = $('<div>')
+							.addClass('gifone');
+					imgContainerOne.append(nounImg);
+					$('#gifsOne').append(imgContainerOne);
+
+					const verbImg = $('<img>')
+							.attr('src', verbData.data[0].images.downsized.url);
+					const imgContainerTwo = $('<div>')
 							.addClass('giftwo');
-					imgContainertwo.append(nounImg);
-					$('#gifsTwo').append(imgContainertwo);
+					imgContainerTwo.append(verbImg);
+					$('#gifsTwo').append(imgContainerTwo);
+
+					const adjImg = $('<img>')
+							.attr('src', adjData.data[0].images.downsized.url);
+					const imgContainerThree = $('<div>')
+							.addClass('gifthree');
+					imgContainerThree.append(adjImg);
+					$('#gifsThree').append(imgContainerThree);
 			
 				});
 			});
 		});
 	});
 });
+
+
