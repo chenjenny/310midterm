41c91c4cceafafcc6998f1c5b147773fefa9a026
= <jenjennychen@gmail.com>
2018-04-18 18:00:43 -0400

    app.js update

--- app.js
+++ app.js
@@ -1,42 +1,56 @@
 // document ready event
-$(document).ready(function() {
-	$('#query').on("keypress", function(event) {
-		if (event.which == 14) {
-			var offset = Math.round(Math.random() * 1000);
-			var query = this.value;
-			var key = "iAqAbqIuPD5ZN9uEq5v3phweMO2u9tda";
-			var url = "https://api.giphy.com/v1/gifs/search?q="
-						+ query
-						+ "&api_key="
-						+ key
-						+ "&limit=20"
-						+ "&offset="
-						+ offset;
+$(document).ready(function () {
+
+	// Noun Gif
+	$('#submit').on("click", function (event) {
+		
+		var key = "iAqAbqIuPD5ZN9uEq5v3phweMO2u9tda";
+		var nounQuery = document.getElementById("queryOne").value;
+		// verb 
+		var verbQuery = document.getElementById("queryOne").value;
+		// adj
+		var adjectiveQuery = document.getElementById("queryOne").value;
+
+		var nounUrl = "https://api.giphy.com/v1/gifs/search?q=" +
+			nounQuery +
+			"&api_key=" +
+			key
+			//Limits the # of gif you want shown, currently 1 gif per section
+			+
+			"&limit=1";
+		
+		var verbUrl = "https://api.giphy.com/v1/gifs/search?q=" +
+			nounQuery +
+			"&api_key=" +
+			key
+			//Limits the # of gif you want shown, currently 1 gif per section
+			+
+			"&limit=1";
+		
+			var adjectiveUrl = "https://api.giphy.com/v1/gifs/search?q=" +
+			nounQuery +
+			"&api_key=" +
+			key
+			//Limits the # of gif you want shown, currently 1 gif per section
+			+
+			"&limit=1";
+		
+		$.getJSON(nounUrl, function (nounData) {
+			$.getJSON(verbUrl, function (verbData) {
+				$.getJSON(adjUrl, function (adjData) {
+					document.getElementById('gifsOne').innerHTML = "";
+					document.getElementById('gifsTwo').innerHTML = "";
+					document.getElementById('gifsThree').innerHTML = "";
+					
+					const nounImg = $('<img>')
+							.attr('src', nounData.data.images[0].downsized.url);
+					const imgContainertwo = $('<div>')
+							.addClass('giftwo');
+					imgContainertwo.append(nounImg);
+					$('#gifsTwo').append(imgContainertwo);
 			
-			$.getJSON(url, function(json) {
-				console.log(json);
-				document.getElementById('gifs').innerHTML = "";
-				for (let i = 0; i < json.data.length; i++) {
-					const img = json.data[i];
-					if (img.images.downsized.url) {
-						const imgElem = $('<img>')
-							.attr('src', img.images.downsized.url);
-					
-						const imgContainer = $('<div>')
-							.addClass('gif');
-					
-						imgContainer.append(imgElem);
-					
-						 $('#gifs').append(imgContainer);
-					}
-				}
+				});
 			});
-		}
+		});
 	});
 });
-
-
-
-
-
-
